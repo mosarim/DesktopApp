d78c0f3d31383a8e79ee604ad879bdc6ee15cde7
Sarim <Sarim@sarims-macbook-pro.local>
2018-10-04 11:14:49 +0100

    Initial Commit

---
+++ DesktopApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesktopApp
+//
+//  Created by Sarim on 04/10/2018.
+//  Copyright © 2018 Sarim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
